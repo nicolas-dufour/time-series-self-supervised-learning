723682a1a5dfbd29fdb7d1be5f23945abe97c4e5
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-04-01 13:15:57 +0200

    Added SIMCLR

--- loss.py
+++ loss.py
@@ -25,3 +25,27 @@ class TripletLoss(nn.Module):
         pos_loss = -torch.log(pos)
 
         return (pos_loss + neg_loss).mean()
+
+
+class TripletLossSIMCLR(nn.Module):
+    def __init__(self, temp=1):
+        super().__init__()
+        self.temp = temp
+        # self.num_negative_samples = num_negative_samples
+
+    def forward(self, batch_emb, pos_batch_emb):
+        batch_size = batch_emb.shape[0]
+
+        batch_emb = batch_emb / batch_emb.norm(dim=-1)[:, None]
+        pos_batch_emb = pos_batch_emb / pos_batch_emb.norm(dim=-1)[:, None]
+
+        cov = torch.mm(batch_emb, batch_emb.t().contiguous())
+        sim = torch.exp(cov/self.temp)
+
+        mask = ~torch.eye(batch_size, device=sim.device).bool()
+        neg = sim.masked_select(mask).view(batch_size, -1)
+        neg_loss = torch.log(torch.sum(neg, dim=-1))
+
+        pos_loss = -torch.sum(batch_emb * pos_batch_emb, dim=-1)/self.temp
+
+        return (pos_loss + neg_loss).mean()
