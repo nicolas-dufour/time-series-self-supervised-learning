2e5c0d51ae9cc3dbfacfc6e921ee3d2a5a4ae6c8
Sam Perochon <52164391+samperochon@users.noreply.github.com>
2021-04-01 16:44:53 +0200

    add nicod changes

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from sklearn.manifold import TSNE
 from sklearn.model_selection import (GridSearchCV,train_test_split, cross_val_score)
 from sklearn.svm import SVC
 import pytorch_lightning as pl
-from loss import TripletLoss, TripletLossSIMCLR
+from loss import TripletLossLogi, TripletLossXent
 from model import CausalCNNEncoder
 from datamodule import UnivariateTestDataset, MultivariateTestDataset
 import plotly.express as px
@@ -11,8 +11,23 @@ import numpy as np
 import pandas as pd
 
 
+
 class TimeSeriesEmbedder(pl.LightningModule):
-    def __init__(self, in_channels, channels, depth, reduced_size, out_channels, kernel_size, lr, weight_decay, betas, train_path, test_path, dataset_name='', multivariate=False, loss_name='TripletLossSIMCLR'):
+    def __init__(
+        self,
+        in_channels,
+        channels,
+        depth,
+        reduced_size,
+        out_channels,
+        kernel_size,
+        lr,
+        weight_decay,
+        betas,
+        multivariate=False,
+        loss="xent",
+        temp=1,
+    ):
         super().__init__()
         self.save_hyperparameters()
         self.encoder = CausalCNNEncoder(
@@ -21,13 +36,16 @@ class TimeSeriesEmbedder(pl.LightningModule):
             depth=depth,
             reduced_size=reduced_size,
             out_channels=out_channels,
-            kernel_size=kernel_size)
-        self.criterium = TripletLoss() if loss_name=='TripletLoss' else TripletLossSIMCLR() if loss_name=='TripletLossSIMCLR' else None
-        self.train_path = train_path
-        self.test_path = test_path
-        self.dataset_name = dataset_name
-        self.multivariate=multivariate
-        self.val_tsne_rep = pd.DataFrame(columns=["labels", "x", "y", "step"])
+            kernel_size=kernel_size,
+        )
+        self.multivariate = multivariate
+#         self.val_tsne_rep = pd.DataFrame(columns=["labels", "x", "y", "step"])
+        if loss == "xent":
+            self.criterium = TripletLossXent(temp=temp)
+        elif loss == "logi":
+            self.criterium = TripletLossLogi(temp=temp)
+        else:
+            raise "Loss not supported"
 
     def forward(self, x):
         return self.encoder(x)
@@ -89,31 +107,35 @@ class TimeSeriesEmbedder(pl.LightningModule):
     #         )
     #         print("Ended")
 
-    # def test_step(self, batch, batch_idx):
-    #     labels, series = batch
-    #     if self.multivariate:
-    #         emb = self(series)
-    #     else:
-    #         emb = self(series[:, None, :])
-    #     return {"labels": labels, "emb": emb.cpu()}
+    def test_step(self, batch, batch_idx):
+        labels, series = batch
+        if self.multivariate:
+            emb = self(series)
+        else:
+            emb = self(series[:, None, :])
+        return {"labels": labels, "emb": emb.cpu()}
 
-    # def test_epoch_end(self, val_step_outputs):
-    #     labels = val_step_outputs[0]["labels"].cpu().numpy()
-    #     embs = val_step_outputs[0]["emb"].cpu().numpy()
+    def test_epoch_end(self, val_step_outputs):
+        labels = val_step_outputs[0]["labels"].cpu().numpy()
+        embs = val_step_outputs[0]["emb"].cpu().numpy()
 
-    #     for i in range(1, len(val_step_outputs)):
-    #         labels = np.concatenate((labels, val_step_outputs[i]["labels"].cpu().numpy()), axis=0)
-    #         embs = np.concatenate((embs, val_step_outputs[i]["emb"].cpu().numpy()), axis=0)
+        for i in range(1, len(val_step_outputs)):
+            labels = np.concatenate(
+                (labels, val_step_outputs[i]["labels"].cpu().numpy()), axis=0
+            )
+            embs = np.concatenate(
+                (embs, val_step_outputs[i]["emb"].cpu().numpy()), axis=0
+            )
 
-    #     tsne = TSNE(n_components=2, random_state=21)
-    #     projected_emb = pd.DataFrame(
-    #         np.concatenate((labels[:, None], tsne.fit_transform(embs)), axis=1),
-    #         columns=["labels", "x", "y"],
-    #     )
-    #     projected_emb["labels"] = projected_emb["labels"].astype("int").astype("str")
-    #     self.log(
-    #         "TSNE_test_proj", px.scatter(projected_emb, x="x", y="y", color="labels")
-    #     )
+        tsne = TSNE(n_components=2, random_state=21)
+        projected_emb = pd.DataFrame(
+            np.concatenate((labels[:, None], tsne.fit_transform(embs)), axis=1),
+            columns=["labels", "x", "y"],
+        )
+        projected_emb["labels"] = projected_emb["labels"].astype("int").astype("str")
+        self.log(
+            "TSNE_test_proj", px.scatter(projected_emb, x="x", y="y", color="labels")
+        )
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
@@ -128,6 +150,7 @@ class TimeSeriesEmbedder(pl.LightningModule):
         '''
             Compute SVM accuracy score on the train then test set. Is sufficient train data, SVM C hyperparameters is found using grid search.  
         '''
+        raise NotImplemented
         train_loader = self.datamodule.val_dataloader()
         test_loader = self.datamodule.test_dataloader()
         train_emb = list()
